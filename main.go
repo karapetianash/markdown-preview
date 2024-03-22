4564798600723c019d637a3ce4647fe4f92a0291
Ashot Karapetian <AshotArtashesi@yandex.ru>
2024-03-22 15:52:35 +0300

    md preview tool with tests (golden file)

--- main.go
+++ main.go
@@ -1,5 +1,73 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+	"os"
+	"path/filepath"
+)
 
+const (
+	header = `<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Markdown Preview Tool</title>
+</head>
+<body>
+`
+	footer = `
+</body>
+</html>
+`
+)
+
+func main() {
+	filename := flag.String("file", "", "Markdown file to preview.")
+	flag.Parse()
+
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(filename string) error {
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
+	fmt.Println(outName)
+
+	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	// Parse the markdown file through blackfriday and bluemonday
+	// to generate a valid and safe HTML
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	var buffer bytes.Buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHTML(outName string, htmlData []byte) error {
+	return os.WriteFile(outName, htmlData, 0664)
 }
